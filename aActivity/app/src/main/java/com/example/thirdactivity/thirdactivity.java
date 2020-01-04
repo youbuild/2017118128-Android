f60110f0cab368e9ae08ed2c6abd466905be16f3
youbuild <2017118128@stu.hstc.edu.cn>
2020-01-04 23:40:34 +0800

    修改

---
+++ aActivity/app/src/main/java/com/example/thirdactivity/thirdactivity.java
@@ -0,0 +1,14 @@
+package com.example.thirdactivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class thirdactivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_thirdactivity);
+    }
+}
